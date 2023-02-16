b08985d69df337355cd7fad70e5a5eda7161121e
jag114 <jag200202@gmail.com>
2023-02-16 18:02:57 +0100

    function to zoom, and scale variable added

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,52 +1,68 @@
 import { useEffect, useRef, useState } from "react";
 import "./Map.css";
 
+const COL_NUMBER = 160;
+const ROW_NUMBER = 80;
+let scale = 1;
+let newMap:any;
+
 function Map() {
-  
-  const [ map, setMap ] = useState(false);
+  // custom hook for this
+  if(localStorage.getItem("mapState") !== null){
+    newMap = [...JSON.parse(localStorage.getItem("mapState")!)]
+  }else{
+    newMap = makeMap();
+  }
+  const [ map, setMap ] = useState([...newMap]) //visible world, changes
+  const [ remake, setRemake ] = useState(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  
+  const worldMap = [...map]; //constant data for world
+  localStorage.setItem("mapState", JSON.stringify(map));
+
   type Cell = { //add terrain type
     x:number, y:number, width:number, height:number,
   }
-  let mapCellsRows = new Array(100);
-  for(let i = 0; i < mapCellsRows.length; i++){
-    mapCellsRows[i] = new Array(100);
-  }
+  
+  function makeMap(){
+    let mapCellsRows = new Array(ROW_NUMBER).fill(new Array(COL_NUMBER));
 
-  for (let i = 0; i < mapCellsRows.length; i++) {
-    for(let j = 0; j < mapCellsRows.length; j++){
-      let cell:Cell = {
-        x: i,
-        y: j,
-        width: 10,
-        height: 10,
-      };
-      mapCellsRows[i][j] = cell;
+    for (let i = 0; i < ROW_NUMBER; i++) {
+      for(let j = 0; j < COL_NUMBER; j++){
+        let cell:Cell = {
+          x: i,
+          y: j,
+          width: 10,
+          height: 10,
+        };
+        mapCellsRows[i][j] = cell;
+      }
     }
+    return mapCellsRows;
   }
 
   const colorArray = ['gray', 'green', 'blue', 'yellow'];
   //randomized colors
-  function draw(ctx:CanvasRenderingContext2D, cell:Cell, i:number, j:number){
+  function draw(ctx:CanvasRenderingContext2D | undefined, cell:Cell, i:number, j:number){
     const { width, height } = cell;
-    ctx.beginPath()
-    //add perlin noise or weights to certain colours aka terrains
-    let index = Math.floor(Math.random() * colorArray.length);
-    ctx.fillStyle = colorArray[index];
-    ctx.fillRect(i * 10 + 10, j * 10 + 10, width, height);
-    ctx.closePath();
+    if(ctx !== undefined){
+      ctx.beginPath()
+      //add perlin noise or weights to certain colours aka terrains
+      let index = Math.floor(Math.random() * colorArray.length);
+      ctx.fillStyle = colorArray[index];
+      ctx.fillRect(i * 10 * scale, j * 10 * scale, width, height);
+      ctx.closePath();
+    }
   }
 
+  const canvas:HTMLCanvasElement | null = canvasRef.current;
+  const context = canvas?.getContext('2d');
   //https://stackoverflow.com/questions/47774145/creating-chess-board-with-canvas
   //when player moves the map its should redraw, make 1 cell bigger visually
   useEffect(() => {
-    const canvas:HTMLCanvasElement | null = canvasRef.current;
     if(canvas !== null){
-      const context = canvas.getContext('2d');
       if(context !== null){
         let start = Date.now();
-        mapCellsRows.forEach((col, j) => {
+        map.forEach((col, j) => {
           col.forEach((cell:Cell, i:number) => {
             draw(context, cell, i, j);
           });
@@ -56,12 +72,41 @@ function Map() {
     }
   }, [draw])
   
+  // needs an algorithm to create new array of cells that are visible after resizing
+  function changeScale(value:string, scaleValue:number, ctx:CanvasRenderingContext2D | null | undefined){
+    let newScale = scaleValue;
+    switch (value) {
+      case "+":
+        if(newScale < 100){
+          newScale++;
+        }
+        break;
+      case "-":
+        if(newScale > 1){
+          newScale--;
+        }
+        break;
+      default:
+        break;
+    }
+
+    if(scale !== newScale){
+      scale = newScale;
+      ctx?.clearRect(0, 0, 1600, 800);
+      setRemake(prevRemake => !prevRemake);
+    }
+  }
+  
   return (
-    <>
-      <canvas id="map-canvas" height="1080px" width="1920px" ref={canvasRef}/>
-      <button onClick={() => setMap(prevMap => !prevMap)}> New map </button>
-    </>
-    
+    <div className="map-holder">
+      <div className="map-zoom">
+        <span> Zoom in/out </span>
+        <button className="map-zoom-plus" onClick={() => changeScale("+", scale, context)}> + </button>
+        <button className="map-zoom-minus" onClick={() => changeScale("-", scale, context)}> - </button>
+      </div>
+      <button className="map-button" onClick={() => setRemake(prevRemake => !prevRemake)}> New map </button>
+      <canvas className="map-canvas" height="800px" width="1600px" ref={canvasRef}/>
+    </div> 
   )
 }
 
