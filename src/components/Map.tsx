82f1dbb0976cc5ef819eed307b6b9570afbba29e
jag114 <jag200202@gmail.com>
2023-02-26 18:21:45 +0100

    cleaning up code

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,7 +13,6 @@ const ROW_NUMBER = 81;
 let scale = 1;
 
 function Map() {
-  //const [ map, setMap ] = useMapState(ROW_NUMBER, COL_NUMBER);
   const [map, setMap] = useState(() => {
     const storedMap = localStorage.getItem("myMap");
     if (storedMap !== null && storedMap.length > 10) {
@@ -23,13 +22,17 @@ function Map() {
     localStorage.setItem("myMap", JSON.stringify(newMap));
     return newMap;
   });
-
+  //save new version of map everytime it changes
   useEffect(() => {
     localStorage.setItem("myMap", JSON.stringify(map));
   }, [map]);
 
-  const [ remake, setRemake ] = useState(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const getContext = (canvas: HTMLCanvasElement | null): CanvasRenderingContext2D | null => {
+    return canvas?.getContext('2d') ?? null;
+  };
+
+  const [ remake, setRemake ] = useState(false); //for forcing rerenders
   const worldMap = [{...map}]; //constant data for world
   //randomized colors
   function draw(ctx:CanvasRenderingContext2D | undefined, cell:Cell, i:number, j:number){
@@ -43,28 +46,23 @@ function Map() {
     }
   }
 
-  const canvas2:HTMLCanvasElement | null = canvasRef.current;
-  const context2 = canvas2?.getContext('2d'); 
-  //when player moves the map its should redraw, make 1 cell bigger visually
   useEffect(() => {   
-    const canvas:HTMLCanvasElement | null = canvasRef.current;
-    const context = canvas?.getContext('2d'); 
-    if(canvas !== null){
-      if(context !== null){
-        const start = Date.now();
-        map.forEach((col:Cell[], j:number) => {
-          col.forEach((cell:Cell, i:number) => {
-            draw(context, cell, i, j);
-          });
-        })   
-        console.log("Draw time: " + (Date.now() - start) / 1000 + " sec.");
-      }
+    const canvas = canvasRef.current;
+    const context = getContext(canvas);
+    if(context !== null){
+      const start = Date.now();
+      map.forEach((col:Cell[], j:number) => {
+        col.forEach((cell:Cell, i:number) => {
+          draw(context, cell, i, j);
+        });
+      }, [])   
+      console.log("Draw time: " + (Date.now() - start) / 1000 + " sec.");
     }
-  }, [draw])
+  })
   console.log(map);
   
   // needs an algorithm to create new array of cells that are visible after resizing
-  function changeScale(value:string, scaleValue:number, ctx:CanvasRenderingContext2D | null | undefined){
+  const changeScale = (value:string, scaleValue:number, ctx:CanvasRenderingContext2D | null | undefined) :void => {
     let newScale = scaleValue;
     switch (value) {
       case "+":
@@ -88,7 +86,7 @@ function Map() {
     }
   }
 
-  function remakeMap(){
+  const remakeMap = (): void => {
     localStorage.setItem("myMap", JSON.stringify([]))
     setRemake(prevRemake => !prevRemake)
   }
@@ -97,8 +95,8 @@ function Map() {
     <div className="map-holder">
       <div className="map-zoom">
         <span> Zoom in/out </span>
-        <button className="map-zoom-plus" onClick={() => changeScale("+", scale, context2)}> + </button>
-        <button className="map-zoom-minus" onClick={() => changeScale("-", scale, context2)}> - </button>
+        <button className="map-zoom-plus" onClick={() => changeScale("+", scale, getContext(canvasRef.current))}> + </button>
+        <button className="map-zoom-minus" onClick={() => changeScale("-", scale, getContext(canvasRef.current))}> - </button>
       </div>
       <button className="map-button" onClick={() => remakeMap()}> New map </button>
       <button className="map-button" onClick={() => localStorage.setItem("myMap", JSON.stringify([]))}> Reset saved map</button>
@@ -108,66 +106,3 @@ function Map() {
 }
 
 export default Map;
-
-/*
-const canvasRef = useRef<HTMLCanvasElement>(null);
-
-// Define a function to get the context of a canvas
-const getContext = (canvas: HTMLCanvasElement | null): CanvasRenderingContext2D | null => {
-  return canvas?.getContext('2d') ?? null;
-};
-
-// Define a function to draw the map
-const drawMap = (context: CanvasRenderingContext2D | null, map: Cell[][]): void => {
-  if (context !== null) {
-    const start = Date.now();
-    map.forEach((col:Cell[], j:number) => {
-      col.forEach((cell:Cell, i:number) => {
-        draw(context, cell, i, j);
-      });
-    })   
-    console.log("Draw time: " + (Date.now() - start) / 1000 + " sec.");
-  }
-};
-
-const Map = (): JSX.Element => {
-  const canvas = canvasRef.current;
-  const context = getContext(canvas);
-
-  const [scale, setScale] = useState<number>(1);
-
-  // Define a function to change the scale of the canvas
-  const changeScale = (op: "+" | "-", scale: number, context: CanvasRenderingContext2D | null): void => {
-    if (context !== null) {
-      if (op === "+") {
-        setScale(scale + 0.1);
-      } else {
-        setScale(scale - 0.1);
-      }
-      context.scale(scale, scale);
-      drawMap(context, map);
-    }
-  };
-
-  // Define a function to remake the map
-  const remakeMap = (): void => {
-    const newMap = createMap();
-    setMap(newMap);
-    drawMap(context, newMap);
-  };
-
-  return (
-    <div className="map-holder">
-      <div className="map-zoom">
-        <span> Zoom in/out </span>
-        <button className="map-zoom-plus" onClick={() => changeScale("+", scale, context)}> + </button>
-        <button className="map-zoom-minus" onClick={() => changeScale("-", scale, context)}> - </button>
-      </div>
-      <button className="map-button" onClick={() => remakeMap()}> New map </button>
-      <button className="map-button" onClick={() => localStorage.setItem("myMap", JSON.stringify([]))}> Reset saved map</button>
-      <canvas className="map-canvas" height="800px" width="1600px" ref={canvasRef}/>
-    </div> 
-  );
-};
-
-*/
